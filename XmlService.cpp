c20f9c1bd0ddfd8ceaf78e1c1946af2ec99e5c4f
wont999 <horvist@yandex.ru>
2023-05-31 15:15:26 +0300

    feature-xml

---
+++ XmlService.cpp
@@ -0,0 +1,33 @@
+#include "XmlService.h"
+
+using namespace pugi;
+
+Weather XmlService::getWeather(std::string s)
+{
+    xml_document doc;
+    xml_parse_result result = doc.load_file(s.c_str());
+    if (!result)
+        throw exception("error");
+    xml_node node = doc.child("current");
+    std::string city = node.child("city").
+        attribute("name").
+        as_string(); //  Киров
+    double lon = node.child("city").child("coord").
+        attribute("lon").
+        as_int(); // 49.6601
+    double	lat = node.child("city").child("coord"). // 58.5966
+        attribute("lat").
+        as_int();
+
+    double temperature = node.child("temperature").
+        attribute("value").
+        as_double(); // 5.69
+    std::string weather = node.child("weather").
+        attribute("value").
+        as_string(); // дождь
+    double windSpeed = node.child("wind").
+        child("speed").attribute("value").
+        as_double();// 4.27
+    int clouds = node.child("clouds").attribute("value").as_int(); // 100
+    return Weather(city, lon, lat, temperature, weather, windSpeed, clouds);
+}
---
+++ XmlService.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Service.h"
+#include "Weather.h"
+#include "pugixml-master/src/pugixml.hpp"
+class XmlService :public Service
+{
+public:
+	virtual Weather getWeather(std::string s) override;
+	virtual ~XmlService() {};
+};
+
+
